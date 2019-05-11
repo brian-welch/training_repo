cc25f90e91a940a8adb3cb7df3e52da70824808c
Brian Welch <myself@brianwelch.se>
2019-05-11 14:16:02 +0200

    Found 'WorkoutLogger' module in config/application.rb - changed to TrainingRepo

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module WorkoutLogger
+module TrainingRepo
   class Application < Rails::Application
     config.generators do |generate|
           generate.assets false
