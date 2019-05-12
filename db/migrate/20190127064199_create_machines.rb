07e0215187c23e7ff5698c119e9598e3778d4551
Brian Welch <myself@brianwelch.se>
2019-05-12 14:42:50 +0200

    cleaned migrations and added column to machines table on creation

--- db/migrate/20190127064199_create_machines.rb
+++ db/migrate/20190127064199_create_machines.rb
@@ -4,6 +4,8 @@ class CreateMachines < ActiveRecord::Migration[5.2]
       t.references :brand
       t.string :name
       t.float :mech_ad
+      t.integer :pulley_count
+      t.integer :inherit_weight
 
       t.timestamps
     end
--- db/migrate/20190404033512_add_column_to_machine.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToMachine < ActiveRecord::Migration[5.2]
-  def change
-    add_column :machines, :pulley_count, :integer
-  end
-end
