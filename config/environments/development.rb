0ef7b7bbdfce36119faf9127d31ba1157b86ee3f
Brian Welch <myself@brianwelch.se>
2019-05-15 20:48:43 +0200

    added content to about page fixed UX bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
 
+  config.webpacker.check_yarn_integrity = false
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
@@ -64,4 +65,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
 end
