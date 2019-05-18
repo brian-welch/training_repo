ff3c74316ad1c42a1c1d75fa1c6a7b4a21a251a2
Brian Welch <myself@brianwelch.se>
2019-05-18 06:09:56 +0200

    add default 0 inherit_weight to machine

--- db/migrate/20190127064199_create_machines.rb
+++ db/migrate/20190127064199_create_machines.rb
@@ -5,7 +5,7 @@ class CreateMachines < ActiveRecord::Migration[5.2]
       t.string :name
       t.float :mech_ad
       t.integer :pulley_count
-      t.integer :inherit_weight
+      t.integer :inherit_weight, default: 0
 
       t.timestamps
     end
