0928fa0ddd8686c699f4cda1bf9cb14176ed747e
Brian Welch <myself@brianwelch.se>
2019-05-19 08:59:41 +0200

    added turorial methods for exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -4,6 +4,16 @@ class Exercise < ApplicationRecord
 
   private
 
+
+  def exercise_name
+    exercise.try(:name)
+  end
+
+  def exercise_name=(name)
+    self.exercise = Exercise.find_by_name(name) if name.present?
+  end
+
+
   def self.all_exercise_hash
     all_exercise_inst = self.all
     all_exercise_hash = {}
