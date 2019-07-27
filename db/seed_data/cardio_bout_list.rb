2fcaca7245a752b0e58af43322f7e614b8bf305f
Brian Welch <myself@brianwelch.se>
2019-07-27 11:40:43 +0200

    cardio bout seeds created

---
+++ db/seed_data/cardio_bout_list.rb
@@ -0,0 +1,14 @@
+def cardio_bout_list
+  return [
+    "Jogging",
+    "Biking",
+    "Walking",
+    "Swimming",
+    "Running Intervals",
+    "Eliptical Machine",
+    "StairMaster",
+    "Rowing",
+    "Sports Activity",
+    "Aerobics Class"
+  ]
+end
