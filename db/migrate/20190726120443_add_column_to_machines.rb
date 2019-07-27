99ef8a9f9091beb47702e0e2ba6931df71906342
Brian Welch <myself@brianwelch.se>
2019-07-27 08:12:21 +0200

    syntax error in foreign key references in recent migrations

--- db/migrate/20190726120443_add_column_to_machines.rb
+++ db/migrate/20190726120443_add_column_to_machines.rb
@@ -1,5 +1,5 @@
 class AddColumnToMachines < ActiveRecord::Migration[5.2]
   def change
-    add_reference :machines, :model_series_id, foreign_key: true
+    add_reference :machines, :model_series, foreign_key: true
   end
 end
