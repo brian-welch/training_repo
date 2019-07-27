ec0508874dbf632c6b68a34c02c3d99ed11e9b75
Brian Welch <myself@brianwelch.se>
2019-07-27 14:14:57 +0200

    migrations for cardio bouts complete

--- db/migrate/20190726104709_create_session_cardio_bouts.rb
+++ db/migrate/20190726104709_create_session_cardio_bouts.rb
@@ -3,7 +3,6 @@ class CreateSessionCardioBouts < ActiveRecord::Migration[5.2]
     create_table :session_cardio_bouts do |t|
       t.float :distance
       t.datetime :duration
-      t.string :sub_type
 
       t.references :training_session, foreign_key: true
       t.references :cardio_bout, foreign_key: true
