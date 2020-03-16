7dd6913f57009eda7bb534e7e0d910d13c997d00
Brian Welch <Brian_Welch@Brians-Mac-mini.local>
2020-03-16 06:30:50 +0100

    tweeking user model and devise links also db diagram updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+  #devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable,
+         # :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable#,
+         # :confirmable,
+         # :lockable
 
   has_many :session_sets
   belongs_to :gender
