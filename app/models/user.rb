e7f2a4f69f677a1dbe563d384a403324448c089f
Brian Welch <Brian_Welch@Brians-Mac-mini.local>
2020-03-26 19:40:18 +0100

    units of measure added to registration view

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   #       :recoverable, :rememberable, :validatable
 
   devise :database_authenticatable,
-         # :registerable,
+         :registerable,
          :recoverable,
          :rememberable,
          :validatable#,
