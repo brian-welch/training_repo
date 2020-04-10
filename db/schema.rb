acc8820ad8ef4497da11148c1023e4b0af63ddde
Brian Welch <Brian_Welch@Brians-Mac-mini.local>
2020-04-10 12:29:19 +0200

    migration run to create new model and table for user_weight

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_100621) do
+ActiveRecord::Schema.define(version: 2020_04_10_102609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
