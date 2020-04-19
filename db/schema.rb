8020d208c15ded6444b2eac22a6b234adc53fadd
Brian Welch <myself@brianwelch.se>
2020-04-19 12:03:47 +0200

    foundation for changing controller logic and making new seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_121144) do
+ActiveRecord::Schema.define(version: 2020_04_18_171646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2020_04_18_121144) do
     t.string "instructions"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "unilateral"
   end
 
   create_table "roles", force: :cascade do |t|
