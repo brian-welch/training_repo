b92d679a6675b19d4ef992d3c43bba922cd01a53
Brian Welch <myself@brianwelch.se>
2020-06-20 18:18:55 +0200

    removed unnecessary migration which atempted to remove unset fields from a table

--- db/migrate/20200614071816_remove_measure_units_from_user.rb
+++
@@ -1,6 +0,0 @@
-class RemoveMeasureUnitsFromUser < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :weight_units, :string
-    remove_column :users, :distance_units, :string
-  end
-end
